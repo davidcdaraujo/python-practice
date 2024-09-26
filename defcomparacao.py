cc07e591dce169778f1b16c9988c6a2a1a7bd535
David Costa de Araújo <15dca15@gmail.com>
2024-09-26 12:41:43 -0300

    Add files via upload

---
+++ defcomparacao.py
@@ -0,0 +1,12 @@
+def compara(n1, n2):
+
+    if n1 > n2:
+        return True
+    else:
+        return False
+    
+
+num1 = float(input("Número1: "))
+num2 = float(input("Número2: "))
+
+print(compara(num1, num2))
